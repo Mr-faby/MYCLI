0bdf9d53620e110ebbb76e2671369d339ce237a4
Mr-faby <1450336211@qq.com>
2022-05-24 10:19:40 +0800

    first push

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,5 @@
 <template>
-    <div>
-        hello vuex!
-        <img src="@/assets/image/1.png" />
-    </div>
+  <div class="app">
+    hello app
+  </div>
 </template>
-
-
-<style lang="scss" scoped>
-    div {
-        // background: url('./assets/image/1.png')
-    }
-</style>
